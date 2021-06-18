9ad18ead8d8fe1273f5c6a5e0eebfeb2c1338520
jsotoca <jsotoca@unprg.edu.pe>
2021-06-17 22:16:09 -0500

    Se agrego consumir api con created

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="blog">
     <Titulo texto="Titulo de mi blog" />
+    <!-- <button @click="consumirApi">Consumir api</button> -->
+    <div v-if="articles">
+      <div v-for="article in articles" :key="article.id">
+        {{article.title}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,8 +15,26 @@
 import Titulo from '../components/Titulo.vue';
 
 export default {
+  data() {
+    return {
+      articles: []
+    }
+  },
   components: {
     Titulo
+  },
+  methods:{
+    async consumirApi(){
+      try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+        this.articles = await data.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created () {
+    this.consumirApi();
   }
 }
 </script>
